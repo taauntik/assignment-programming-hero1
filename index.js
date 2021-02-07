89263b7524b24abec4452c14c6daeee92f283e71
taauntik <devauntik@gmail.com>
2021-02-08 05:52:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const foodMenu = document.getElementById("food-menu");
+const foodDetail = document.getElementById("food-detail");
+const searchBar = document.getElementById("search-bar");
+const searchBtn = document.getElementById("searchBtn");
+const card = document.querySelector(".card");
+
+searchBtn.addEventListener("click", () => {
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchBar.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.meals === null) {
+        alert("we don't have that dish");
+      } else {
+        displayCategories(data.meals);
+      }
+    });
+});
+
+const displayCategories = (categories) => {
+  console.log(categories);
+  foodMenu.innerHTML = "";
+  categories.forEach((food) => {
+    const foodItem = document.createElement("div");
+    foodItem.className = "card shadow";
+    const foodInfo = `
+              <img src="${food.strMealThumb}" class="card-img-top" height="200" width="300">
+                  <div class="card-body">
+                      <p class="card-text">${food.strMeal}</p>
+                  </div>
+          `;
+    foodItem.innerHTML = foodInfo;
+    foodMenu.appendChild(foodItem);
+
+    // when a user clicks on a div that have to show.
+    foodItem.addEventListener("click", () => {
+      const foodIngredients = `
+                <img src="${food.strMealThumb}" width="400" height="300">
+                <h2>${food.strMeal}</h2>
+                <div class="Ingredients">
+                    <ul>
+                        <li>${food.strIngredient1}</li>
+                        <li>${food.strIngredient2}</li>
+                        <li>${food.strIngredient3}</li>
+                        <li>${food.strIngredient4}</li>
+                        <li>${food.strIngredient5}</li>
+                        <li>${food.strIngredient6}</li>
+                    <ul>
+                    <button onclick="document.getElementById('food-detail').style.display = 'none'" class="m-5 btn btn-success">GO BACK</button>
+                </div>
+            `;
+      foodDetail.innerHTML = foodIngredients;
+      foodDetail.style.display = "flex";
+    });
+  });
+};
